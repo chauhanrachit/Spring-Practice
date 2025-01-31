c2703a2eb809796263dcebf24d6e5fb1403579cb
Rachit Chauhan <chauhan.rachit21@gmail.com>
2025-01-31 07:27:13 +0530

    Changes/Progress

--- react/01-frontend/todo-app/src/App.js
+++ react/01-frontend/todo-app/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 //import LearningComponent from './components/learning-examples/LearningComponent';
-import Counter from './components/counter/Counter';
+//import Counter from './components/counter/Counter';
+import TodoApp from './components/todo/TodoApp';
 
 function App() {
   return(
     <div className="App">
-      {/*<PlayingWithProps property1="value1" property2="value2"/>*/}
-      <Counter />
+      <TodoApp />
+      {/*<PlayingWithProps property1="value1" property2="value2"/>
+      <Counter /> */}
     </div>
   )
   // return (
