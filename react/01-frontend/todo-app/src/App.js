1c7a1cb17a4eaae620921ba9a5412949170d6c5b
Rachit Chauhan <chauhan.rachit21@gmail.com>
2025-02-03 08:15:26 +0530

    Changes/Progress

--- react/01-frontend/todo-app/src/App.js
+++ react/01-frontend/todo-app/src/App.js
@@ -1,55 +1,11 @@
 import './App.css';
-//import LearningComponent from './components/learning-examples/LearningComponent';
-//import Counter from './components/counter/Counter';
 import TodoApp from './components/todo/TodoApp';
 
 function App() {
   return(
     <div className="App">
       <TodoApp />
-      {/*<PlayingWithProps property1="value1" property2="value2"/>
-      <Counter /> */}
     </div>
   )
-  // return (
-  //   <div className="App">
-  //     <LearningComponent />
-  //   </div>
-  // )
 }
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Parentheses () make returning complex JSX values easier
-//Function Components
-//class components
-// function PlayingWithProps(properties){
-//   console.log(properties)
-//   console.log(properties.property1) 
-//   console.log(properties.property2) 
-//   return(
-//     <div></div>
-//   )
-// }
-// function PlayingWithProps({property1,property2}){
-// //  console.log(properties)
-//   console.log(property1) 
-//   console.log(property2) 
-//   return(
-//     <div></div>
-//   )
-// }
--- react/01-frontend/todo-app/src/components/todo/TodoApp.jsx
+++ react/01-frontend/todo-app/src/components/todo/TodoApp.jsx
@@ -1,34 +1,86 @@
+import { useState } from "react"
+import './TodoApp.css';
+import { BrowserRouter, Route, Routes, useNavigate, useParams } from "react-router-dom";
+
 export default function TodoApp() {
     return(
         <div className="TodoApp">
-            <LoginComponent />
-            {/* <WelcomeComponent /> */}
+            <BrowserRouter>
+                <Routes>
+                    <Route path ='/' element={<LoginComponent />} ></Route>
+                    <Route path ='/login' element={<LoginComponent />} ></Route>
+                    <Route path ='/welcome/:username' element={<WelcomeComponent />} ></Route>
+                    <Route path ='*' element={<ErrorComponent />} ></Route>
+                </Routes>
+            </BrowserRouter>
         </div>
     )
 }
-
 function LoginComponent() {
+    const [username,setUsername] =useState('rachit');
+    const [password,setPassword] =useState('');
+    const [showSuccessMessage,setShowSuccessMessage] =useState(false);
+    const [showErrorMessage,setShowErrorMessage] =useState(false);
+    const navigate = useNavigate();
+    function handleUserNameChange(event){
+        setUsername(event.target.value);
+    }
+    function handlePasswordChange(event){
+        setPassword(event.target.value);
+    }
+    function handleSubmit(){
+        if(username==='rachit' && password==='rachit'){
+            setShowSuccessMessage(true);
+            setShowErrorMessage(false);
+            navigate(`/welcome/${username}`);
+        }
+        else {
+            setShowSuccessMessage(false);
+            setShowErrorMessage(true);
+        }
+    }
     return(
         <div className="Login">
+            <h1>Login Page</h1>
+            {showSuccessMessage && <div className="successMessage">Authenticated Successfully.</div> }
+            {showErrorMessage && <div className="errorMessage">Authenticatation Failed. Please ckeck your credentials.</div> }
             <div className="LoginForm">
                 <div>
                     <label>User Name</label>
-                    <input type="text" name="username" value="Rachit"/>
+                    <input type="text" name="username" value={username} onChange={handleUserNameChange}/>
                 </div>
                 <div>
                     <label>Password</label>
-                    <input type="password" name="password" />
+                    <input type="password" name="password" value={password} onChange={handlePasswordChange}/>
                 </div>
                 <div>
-                    <button type="button" name="login">Login</button>
+                    <button type="button" name="login" onClick={handleSubmit}>Login</button>
                 </div>
             </div>
         </div>
     )
 }
-
 function WelcomeComponent() {
+
+    // const params = useParams();
+    // console.log(params.username);
+    const {username} = useParams();
+    console.log(username);
+
     return(
-        <div className="Welcome">Welcome Component</div>
+        <div className="WelcomeComponent">
+            <h1>Welcome {username}</h1>
+            <div>Welcome Component</div>
+        </div>
+    )
+}
+function ErrorComponent() {
+    return(
+        <div className="ErrorComponent">
+            <h1>We are working really hard!</h1>
+            <div>
+                Apologies for the 404. Reach out to our team at ABC-DEF-_GHIJ.
+            </div>
+        </div>
     )
 }
