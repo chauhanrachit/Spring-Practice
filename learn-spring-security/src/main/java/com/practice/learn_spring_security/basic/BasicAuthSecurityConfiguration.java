3668f4bf65d40a46bb84eacf4415588417cdda5d
Rachit Chauhan <chauhan.rachit21@gmail.com>
2025-02-28 06:58:46 +0530

    Changes/Progress

--- learn-spring-security/src/main/java/com/practice/learn_spring_security/basic/BasicAuthSecurityConfiguration.java
+++ learn-spring-security/src/main/java/com/practice/learn_spring_security/basic/BasicAuthSecurityConfiguration.java
@@ -8,23 +8,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity(jsr250Enabled = true,securedEnabled = true)
 public class BasicAuthSecurityConfiguration {
 
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		
-		http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+		http.authorizeHttpRequests(
+				auth -> {
+						auth
+						.requestMatchers("/users").hasRole("USER")
+						.requestMatchers("/admin/**").hasRole("ADMIN")
+						.anyRequest().authenticated();
+				});
 		//http.formLogin(withDefaults());
 		http.sessionManagement(
 					session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -33,33 +42,7 @@ public class BasicAuthSecurityConfiguration {
 		http.csrf().disable();
 		http.headers().frameOptions().sameOrigin();
 		return http.build();
-		
-//		http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
-//		http.formLogin(withDefaults());
-//		http.httpBasic(withDefaults());
-//		return http.build();
 	}
-	
-//	@Bean
-//	public UserDetailsService userDetailsService() {
-//		
-//		var user = User.withUsername("rachit")
-//			.password("{noop}pass")
-//			.roles("USER")
-//			.build();
-//		var admin = User.withUsername("admin")
-//				.password("{noop}pass")
-//				.roles("ADMIN")
-//				.build();
-//		
-//		return new InMemoryUserDetailsManager(user,admin);
-//	}
-	//For storing User details in the DB --
-	//ctrl+shift+t -> go to JdbcDaoImpl class
-	//From there we can get DEFAULT_USER_SCHEMA_DDL_LOCATION 
-	//(this is the path of a ddl which contains schema for creating a User table in db)
-	//And we want it to be executed on startup of our application
-	
 	@Bean
 	public DataSource dataSource() {
 		return new EmbeddedDatabaseBuilder()
@@ -72,11 +55,15 @@ public class BasicAuthSecurityConfiguration {
 	public UserDetailsService userDetailsService(DataSource dataSource) {
 		
 		var user = User.withUsername("rachit")
-			.password("{noop}pass")
-			.roles("USER")
-			.build();
+//				.password("{noop}pass")
+				.password("pass")
+				.passwordEncoder(str ->passwordEncoder().encode(str))
+				.roles("USER")
+				.build();
 		var admin = User.withUsername("admin")
-				.password("{noop}pass")
+//			.password("{noop}pass")
+				.password("pass")
+				.passwordEncoder(str ->passwordEncoder().encode(str))
 				.roles("ADMIN","USER")
 				.build();
 		
@@ -85,5 +72,10 @@ public class BasicAuthSecurityConfiguration {
 		jdbcUserDetailsManager.createUser(admin);
 		
 		return jdbcUserDetailsManager;
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(10);
 	}
 }
